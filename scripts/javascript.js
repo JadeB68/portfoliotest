2b9b16f6b1bb60b85666fe2dbd021bc16c6f99ee
JadeB68 <60108410+JadeB68@users.noreply.github.com>
2022-01-12 15:25:11 +0100

    Add files via upload

---
+++ scripts/javascript.js
@@ -0,0 +1,39 @@
+document.querySelector(".menu").addEventListener("click", function(){
+    document.querySelector(".bg1").classList.toggle("ouvert");
+    document.querySelector(".bg2").classList.toggle("ouvertdelay1");
+    document.querySelector("nav").classList.toggle("ouvertdelay2");
+    document.querySelector(".menu").classList.toggle("menuouvert");
+    document.querySelector(".header").classList.toggle("headerouvert");
+});
+
+document.querySelector(".dir").addEventListener("click", function(){
+    document.querySelector(".bg1").classList.toggle("ouvert");
+    document.querySelector(".bg2").classList.toggle("ouvertdelay1");
+    document.querySelector("nav").classList.toggle("ouvertdelay2");
+    document.querySelector(".menu").classList.toggle("menuouvert");
+    document.querySelector(".header").classList.toggle("headerouvert");
+});
+
+document.querySelector(".dir2").addEventListener("click", function(){
+    document.querySelector(".bg1").classList.toggle("ouvert");
+    document.querySelector(".bg2").classList.toggle("ouvertdelay1");
+    document.querySelector("nav").classList.toggle("ouvertdelay2");
+    document.querySelector(".menu").classList.toggle("menuouvert");
+    document.querySelector(".header").classList.toggle("headerouvert");
+});
+
+document.querySelector(".dir3").addEventListener("click", function(){
+    document.querySelector(".bg1").classList.toggle("ouvert");
+    document.querySelector(".bg2").classList.toggle("ouvertdelay1");
+    document.querySelector("nav").classList.toggle("ouvertdelay2");
+    document.querySelector(".menu").classList.toggle("menuouvert");
+    document.querySelector(".header").classList.toggle("headerouvert");
+});
+
+document.querySelector(".dir4").addEventListener("click", function(){
+    document.querySelector(".bg1").classList.toggle("ouvert");
+    document.querySelector(".bg2").classList.toggle("ouvertdelay1");
+    document.querySelector("nav").classList.toggle("ouvertdelay2");
+    document.querySelector(".menu").classList.toggle("menuouvert");
+    document.querySelector(".header").classList.toggle("headerouvert");
+});
