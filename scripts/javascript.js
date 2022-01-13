8e5460cc8b7baaacb1bc2ae5d4e2e01a23597bc6
JadeB68 <60108410+JadeB68@users.noreply.github.com>
2022-01-13 18:56:43 +0100

    Add files via upload

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -37,3 +37,19 @@ document.querySelector(".dir4").addEventListener("click", function(){
     document.querySelector(".menu").classList.toggle("menuouvert");
     document.querySelector(".header").classList.toggle("headerouvert");
 });
+
+function setHalfVolume() {
+    var myAudio = document.getElementById("audio1");  
+    myAudio.volume = 0.5; //Changed this to 0.5 or 50% volume since the function is called Set Half Volume ;)
+}
+
+
+
+/*
+var audio = $("#audio")[0];
+$("#son").mouseenter(function() {
+audio.play();
+});
+*/
+
+
